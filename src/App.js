a9502e7719fb863b1d971a64b76509319e97206b
AXO8GQZ_thdgit <ALAN_DANIEL_OCHOA_VILLEGAS@homedepot.com>
2022-05-18 16:07:01 -0500

    first commit start files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <div className='App'>
+      <Typography variant="h1" component="div" gutterBottom>
+      Contributions Manager
+      </Typography>
+    </div>
+  )
+}
+
+export default App
